a50c072992393a8febbd69fa0a7e42d3dc17a297
ARATI-UPARE <aratiupare@gmail.com>
2020-04-05 17:10:01 +0530

    ADDED:Given Three Floats Find Maximum Number

--- src/main/java/com/findmaximumgeneric/FindMaximum.java
+++ src/main/java/com/findmaximumgeneric/FindMaximum.java
@@ -6,16 +6,29 @@ public class FindMaximum {
     public FindMaximum(){
 
     }
-    // Method to find Maximum Number
+    // Method to find Maximum IntegerNumber
     public static int findMaxInteger(int no1, int no2, int no3) {
 
         int maxInt = 0;
-        if (no1 > no2 && no1 > no2) {
+        if (no1 > no2 && no1 > no3) {
             return maxInt = no1;
         } else if (no2 > no1 && no2 > no3) {
             return maxInt = no2;
         } else {
             return maxInt = no3;
+        }
+    }
+
+    // Method to find Maximum Float Number
+    public static double findMaxFloat(double fNo1, double fNo2, double fNo3) {
+
+        double maxFloat= 0;
+        if (fNo1 > fNo2 && fNo1 > fNo3) {
+            return maxFloat = fNo1;
+        } else if (fNo2 > fNo1 && fNo2 > fNo3) {
+            return maxFloat = fNo2;
+        } else {
+            return maxFloat = fNo3;
         }
     }
 }
--- src/test/java/com/findmaximumgeneric/FindMaximumTest.java
+++ src/test/java/com/findmaximumgeneric/FindMaximumTest.java
@@ -24,5 +24,26 @@ public class FindMaximumTest {
         Comparable maxNumber = findMax.findMaxInteger(10,20,30);
         Assert.assertEquals(30, maxNumber);
     }
+    //TC:2.1
+    @Test
+    public void givenMaxFloatAtFirstPosition_shouldReturnFirstPosition() {
+        double maxFloat = findMax.findMaxFloat(30.5,20.5,10.5);
+        Assert.assertEquals(30.5, maxFloat, 0.0);
+    }
+    //TC:2.2
+    @Test
+    public void givenMaxFloatAtSecondPosition_shouldReturnFirstPosition() {
+        FindMaximum findMaximum = new FindMaximum();
+        double maxFloat = findMaximum.findMaxFloat(10.5,30.5,20.5);
+        Assert.assertEquals(30.5, maxFloat, 0.0);
+    }
+    //TC:2.3
+    @Test
+    public void givenMaxFloatAtThirdPosition_shouldReturnFirstPosition() {
+        FindMaximum findMaximum = new FindMaximum();
+        double maxFloat = findMaximum.findMaxFloat(10.5, 20.5, 30.5);
+        Assert.assertEquals(30.5, maxFloat, 0.0);
+    }
 }
+
 
