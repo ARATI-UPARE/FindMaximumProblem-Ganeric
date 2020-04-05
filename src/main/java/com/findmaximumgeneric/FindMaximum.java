6a3de29ec37ef7415596eea0051d7fb99eb2e8f2
ARATI-UPARE <aratiupare@gmail.com>
2020-04-05 16:50:05 +0530

    ADDED:Given Three Integer Find Maximum Number

--- src/main/java/com/findmaximumgeneric/FindMaximum.java
+++ src/main/java/com/findmaximumgeneric/FindMaximum.java
@@ -1,5 +1,26 @@
 package com.findmaximumgeneric;
-
+// WELCOME To Find Maximum Number using GENERIC
 public class FindMaximum {
 
+    // Default Constructor
+    public FindMaximum(){
+
+    }
+    // Method to find Maximum Number
+    public static int findMaxInteger(int no1, int no2, int no3) {
+
+        int maxInt = 0;
+        if (no1 > no2 && no1 > no2) {
+            return maxInt = no1;
+        } else if (no2 > no1 && no2 > no3) {
+            return maxInt = no2;
+        } else {
+            return maxInt = no3;
+        }
+    }
 }
+
+
+
+
+
--- src/test/java/com/findmaximumgeneric/FindMaximumTest.java
+++ src/test/java/com/findmaximumgeneric/FindMaximumTest.java
@@ -1,4 +1,28 @@
 package com.findmaximumgeneric;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class FindMaximumTest {
+
+    FindMaximum findMax = new FindMaximum();
+    //TC:1.1
+    @Test
+    public void givenMaxIntegerAtFirstPosition_shouldReturnFirstPosition() {
+        Comparable maxNumber = findMax.findMaxInteger(30,10,20);
+        Assert.assertEquals(30, maxNumber);
+    }
+    //TC:1.2
+    @Test
+    public void givenMaxIntegerAtSecondPosition_shouldReturnSecondPosition() {
+        Comparable maxNumber = findMax.findMaxInteger(10,30,20);
+        Assert.assertEquals(30, maxNumber);
+    }
+    //TC:1.3
+    @Test
+    public void givenMaxIntegerAtThirdPosition_shouldReturnThirdPosition() {
+        Comparable maxNumber = findMax.findMaxInteger(10,20,30);
+        Assert.assertEquals(30, maxNumber);
+    }
 }
+
